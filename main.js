eb4cb79e1b65cec6a2721374dc08f7dcfa4028a4
KrystineJannenga <kjannenga1@gmail.com>
2017-03-15 16:10:12 -0700

    finished

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ $(document).ready(function(){
     var seconds = currentTime.getSeconds()
     setInterval(startClock, 1000)
 
+
     if (seconds < 10) {
     seconds = "0" + seconds;
     } if (minutes < 10) {
-    seconds = "0" + minutes;
+    minutes = "0" + minutes;
     } if (hours < 10) {
-    seconds = "0" + hours;
+    hours = "0" + hours;
     }
 
-    if (hours > 12) {
+   
+ if (hours > 12) {
     hours = hours - 12;
     }
     if (hours === 0) {
